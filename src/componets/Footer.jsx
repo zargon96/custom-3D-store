35e3a24798de6e762fcd79f12bc0b3267183ac02
marco <marco.crucitti96@gmail.com>
2026-03-26 13:45:17 +0100

    modificata data del footer

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -1,11 +1,9 @@
 import "../styles/footer.css";
 export default function Footer({ variant }) {
-  const currentYear = new Date().getFullYear();
   return (
     <>
       <footer className={`footer footer-${variant}`}>
-        © <time dateTime={currentYear}>{currentYear}</time> Negozio Nike All
-        rights reserved.
+        © <time dateTime="2025">2025</time> Negozio Nike All rights reserved.
         <a
           href="/credits"
           target="_blank"
